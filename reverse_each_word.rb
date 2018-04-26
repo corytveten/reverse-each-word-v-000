135de4a069ce108c1cd5ecaa6298ca1b8ddca79f
corytveten <corytveten@gmail.com>
2018-04-26 12:05:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(' ')
-  sentence_array.each { |word|
+  new_array = []
+  new_array = sentence_array.each { |word|
   word.reverse
 }.join(" ")
 end
